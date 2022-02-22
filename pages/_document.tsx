d1be7e952166d35a9182c5cac6e641679a0de20d
Kiran <kiran@boxyhq.com>
2022-02-22 11:44:12 +0530

    Applied prettier

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,18 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
           <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;700;800&display=swap"
-            rel="stylesheet"
+            href='https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;700;800&display=swap'
+            rel='stylesheet'
           />
         </Head>
-        <body className="theme-default">
+        <body className='theme-default'>
           <Main />
           <NextScript />
         </body>
