dae3b90fb9d30072501af8720c59546ce6b9febb
Kiran <kiran@boxyhq.com>
2022-02-22 11:35:21 +0530

    Fix warning on default export

--- lib/env.ts
+++ lib/env.ts
@@ -1,11 +1,11 @@
-// import { fetchPrivateKey, fetchPublicKey } from "utils";
-
 const appUrl = process.env.APP_URL || 'http://localhost:4000';
 const entityId = process.env.ENTITY_ID || 'http://saml.example.com';
 const ssoUrl = `${appUrl}/api/saml/sso`;
 
-export default {
+const config = {
   appUrl,
   entityId,
   ssoUrl,
 };
+
+export default config;
