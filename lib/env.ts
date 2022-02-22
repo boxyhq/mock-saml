364cd92ffb570501b1ccf8142b9a07e029f499d0
Kiran <kiran@boxyhq.com>
2022-02-22 11:30:14 +0530

    Add response signing - wip

--- lib/env.ts
+++ lib/env.ts
@@ -1,3 +1,5 @@
+// import { fetchPrivateKey, fetchPublicKey } from "utils";
+
 const appUrl = process.env.APP_URL || 'http://localhost:4000';
 const entityId = process.env.ENTITY_ID || 'http://saml.example.com';
 const ssoUrl = `${appUrl}/api/saml/sso`;
