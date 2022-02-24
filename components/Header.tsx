e6c94bc300fdb1342d62a419ecc2002890ca6ce5
Aswin V <vaswin91@gmail.com>
2022-02-24 23:46:45 +0530

    Automatic class sorting with prettier

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,10 +2,10 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className='text-gray-600 body-font'>
-      <div className='container flex flex-col flex-wrap items-center p-5 mx-auto md:flex-row'>
+    <header className='body-font text-gray-600'>
+      <div className='container mx-auto flex flex-col flex-wrap items-center p-5 md:flex-row'>
         <Link href='/'>
-          <a className='flex items-center mb-4 font-medium text-gray-900 title-font md:mb-0'>
+          <a className='title-font mb-4 flex items-center font-medium text-gray-900 md:mb-0'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
               fill='none'
@@ -13,7 +13,7 @@ export default function Header() {
               strokeLinecap='round'
               strokeLinejoin='round'
               strokeWidth={2}
-              className='w-10 h-10 p-2 text-white bg-indigo-500 rounded-full'
+              className='h-10 w-10 rounded-full bg-indigo-500 p-2 text-white'
               viewBox='0 0 24 24'>
               <path d='M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5' />
             </svg>
