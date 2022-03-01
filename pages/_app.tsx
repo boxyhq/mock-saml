076949f85659fc7b09b7a0e2b56699fb15597425
Deepak Prabhakara <deepak@boxyhq.com>
2022-03-01 00:50:36 +0000

    added Google Tag Manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import 'styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from 'components/Layout';
+import TagManager from 'react-gtm-module';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (process.env.NEXT_PUBLIC_GTM_ID) {
+      TagManager.initialize({ gtmId: process.env.NEXT_PUBLIC_GTM_ID });
+    }
+  }, []);
   return (
     <Layout>
       <Component {...pageProps} />
