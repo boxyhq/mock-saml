b49f022c217ab3935b8cb9488fd5b0227cfffa65
Aswin V <vaswin91@gmail.com>
2022-03-03 23:49:32 +0530

    Style and layout tweaks

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className='body-font text-gray-600'>
       <div className='container mx-auto flex flex-col flex-wrap items-center p-5 md:flex-row'>
         <Link href='/'>
-          <a className='title-font mb-4 flex items-center font-medium text-gray-900 md:mb-0'>
+          <a className='title-font flex items-center font-medium text-gray-900 md:mb-0'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
               fill='none'
@@ -17,22 +17,22 @@ export default function Header() {
               viewBox='0 0 24 24'>
               <path d='M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5' />
             </svg>
-            <span className='ml-3 text-xl'>Mock SAML</span>
+            <span className='ml-3 text-2xl'>Mock SAML</span>
           </a>
         </Link>
-        <span className='ml-2 text-sm text-gray-500'>
-          Powered by
+        <nav className='mt-2 flex flex-col flex-wrap items-center text-base md:ml-auto md:mt-0 md:items-end'>
+          <span className='ml-2 text-sm text-gray-500 '>
+            Made with <span className='text-[#e25555]'>&#9829;</span>
+            <a
+              href='https://boxyhq.com/'
+              className='ml-1 text-gray-600'
+              rel='noopener noreferrer'
+              target='_blank'>
+              <strong>BoxyHQ</strong>
+            </a>
+          </span>
           <a
-            href='https://boxyhq.com/'
-            className='ml-1 text-gray-600'
-            rel='noopener noreferrer'
-            target='_blank'>
-            <strong>BoxyHQ</strong>
-          </a>
-        </span>
-        <nav className='flex flex-wrap items-center justify-center text-base md:ml-auto'>
-          <a
-            className='mr-5 underline hover:text-gray-900'
+            className='leading-5 underline hover:text-gray-900'
             href='https://github.com/boxyhq/jackson'
             rel='noopener noreferrer'
             target='_blank'>
