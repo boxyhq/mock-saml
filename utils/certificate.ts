435e193b61eb582125a92f329a1d6d22f962ab7b
Deepak Prabhakara <deepak@boxyhq.com>
2022-04-10 21:49:44 +0100

    cleaned up node-forge

--- utils/certificate.ts
+++ utils/certificate.ts
@@ -1,6 +1,3 @@
-import { asn1, pki, util } from 'node-forge';
-import saml from '@boxyhq/saml20';
-
 const fetchPublicKey = (): string => {
   return process.env.PUBLIC_KEY ? Buffer.from(process.env.PUBLIC_KEY!, 'base64').toString('ascii') : '';
 };
