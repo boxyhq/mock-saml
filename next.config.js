40b2d0e41bc75d1882d09ed328eb671b553c14fa
Deepak Prabhakara <deepak@boxyhq.com>
2023-10-29 00:21:15 +0100

    turn off esmExternals

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  experimental: { esmExternals: false, webpackBuildWorker: true },
   reactStrictMode: true,
   output: 'standalone',
   webpack: (config, { isServer }) => {
