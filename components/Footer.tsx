7dd05df751b9e0433e135339e1900fe4f315098d
Deepak Prabhakara <deepak@boxyhq.com>
2025-05-29 12:30:20 +0100

    tweak

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -19,26 +19,13 @@ export default function Footer() {
         <p className='mt-4 text-sm text-gray-500 sm:ml-4 sm:mt-0 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:pl-4'>
           Powered by
           <a
-            href='https://boxyhq.com/'
+            href='https://ory.sh'
             className='ml-1 text-gray-600'
             rel='noopener noreferrer'
             target='_blank'>
-            <strong>BoxyHQ</strong>
+            <strong>Ory</strong>
           </a>
         </p>
-        <span className='mt-4 inline-flex justify-center sm:ml-auto sm:mt-0 sm:justify-start'>
-          <a className='ml-3 text-gray-500'>
-            <svg
-              fill='currentColor'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth={2}
-              className='h-5 w-5'
-              viewBox='0 0 24 24'>
-              <path d='M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z' />
-            </svg>
-          </a>
-        </span>
       </div>
     </footer>
   );
